82b821a1b75a4c526e4dd5f15ffa2ca8237d514b
Dhruv1170 <dhruv.frontendarmy@gmail.com>
2022-03-31 16:57:29 +0530

    dto change

--- src/main/java/com/shopin_product_API/entity/ProductDto.java
+++ src/main/java/com/shopin_product_API/entity/ProductDto.java
@@ -1,31 +1,66 @@
 package com.shopin_product_API.entity;
 
+
 public class ProductDto {
-
-    private Long productid;
-
-    private Long userid;
-
-    public ProductDto() {
-    }
-
-    public Long getUserid() {
-        return userid;
-    }
-
-    public void setUserid(Long userid) {
-        this.userid = userid;
-    }
-
-    public Long getProductid() {
-        return productid;
-    }
-
-    public ProductDto(Long productid) {
-        this.productid = productid;
-    }
+    private  Long productid;
+    private  String pname;
+    private String image;
+    private String brandName;
+    private String pgender;
+    private Integer price;
+    private Long userId;
 
     public void setProductid(Long productid) {
         this.productid = productid;
     }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+
+    public String getPname() {
+        return pname;
+    }
+
+    public void setPname(String pname) {
+        this.pname = pname;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getBrandName() {
+        return brandName;
+    }
+
+    public void setBrandName(String brandName) {
+        this.brandName = brandName;
+    }
+
+    public String getPgender() {
+        return pgender;
+    }
+
+    public void setPgender(String pgender) {
+        this.pgender = pgender;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
 }
